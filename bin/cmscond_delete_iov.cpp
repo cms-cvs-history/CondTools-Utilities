9c01522f05e09708015805095c7d49c6b9b0e1ef
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-10-25 17:10:41 +0000

    add delete tool

---
+++ bin/cmscond_delete_iov.cpp
@@ -0,0 +1,117 @@
+#include "CondCore/DBCommon/interface/ServiceLoader.h"
+#include "CondCore/DBCommon/interface/DBSession.h"
+#include "CondCore/DBCommon/interface/Exception.h"
+#include "CondCore/MetaDataService/interface/MetaData.h"
+#include "CondCore/IOVService/interface/IOVService.h"
+#include "CondCore/IOVService/interface/IOVEditor.h"
+#include <boost/program_options.hpp>
+int main( int argc, char** argv ){
+  boost::program_options::options_description desc("options");
+  boost::program_options::options_description visible("Usage: cmscond_delete_iov [options] \n");
+  visible.add_options()
+    ("connect,c",boost::program_options::value<std::string>(),"connection string(required)")
+    ("user,u",boost::program_options::value<std::string>(),"user name (default \"\")")
+    ("pass,p",boost::program_options::value<std::string>(),"password (default \"\")")
+    ("catalog,f",boost::program_options::value<std::string>(),"file catalog contact string (default $POOL_CATALOG)")
+    ("all,a","delete all tags")
+    ("tag,t",boost::program_options::value<std::string>(),"list info of the specified tag")
+    ("help,h", "help message")
+    ;
+  desc.add(visible);
+  boost::program_options::variables_map vm;
+  try{
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).run(), vm);
+    boost::program_options::notify(vm);
+  }catch(const boost::program_options::error& er) {
+    std::cerr << er.what()<<std::endl;
+    return 1;
+  }
+  if (vm.count("help")) {
+    std::cout << visible <<std::endl;;
+    return 0;
+  }
+  std::string connect;
+  std::string catalog("file:PoolFileCatalog.xml");
+  std::string user("");
+  std::string pass("");
+  bool deleteAll=true;
+  std::string tag;
+  if(!vm.count("connect")){
+    std::cerr <<"[Error] no connect[c] option given \n";
+    std::cerr<<" please do "<<argv[0]<<" --help \n";
+    return 1;
+  }else{
+    connect=vm["connect"].as<std::string>();
+  }
+  if(vm.count("user")){
+    user=vm["user"].as<std::string>();
+  }
+  if(vm.count("pass")){
+    pass=vm["pass"].as<std::string>();
+  }
+  if(vm.count("catalog")){
+    catalog=vm["catalog"].as<std::string>();
+  }
+  if(vm.count("tag")){
+    tag=vm["tag"].as<std::string>();
+    deleteAll=false;
+  }
+  cond::ServiceLoader* loader=new cond::ServiceLoader;
+  std::string userenv(std::string("CORAL_AUTH_USER=")+user);
+  std::string passenv(std::string("CORAL_AUTH_PASSWORD=")+pass);
+  ::putenv(const_cast<char*>(userenv.c_str()));
+  ::putenv(const_cast<char*>(passenv.c_str()));
+  loader->loadAuthenticationService(cond::Env);
+  loader->loadMessageService(cond::Error);
+  if( deleteAll ){
+    try{
+      cond::DBSession session(connect,catalog);
+      cond::IOVService iovservice(session);
+      session.connect(cond::ReadWrite);
+      session.startUpdateTransaction();
+      iovservice.deleteAll();
+      session.commit();
+      session.disconnect();
+      cond::MetaData metadata_svc(connect, *loader);
+      metadata_svc.connect(cond::ReadWrite);
+      metadata_svc.deleteAllEntries();
+      metadata_svc.disconnect();
+      return 0;
+    }catch(cond::Exception& er){
+      std::cout<<er.what()<<std::endl;
+    }catch(std::exception& er){
+      std::cout<<er.what()<<std::endl;
+    }catch(...){
+      std::cout<<"Unknown error"<<std::endl;
+    }
+  }else{
+    try{
+      cond::MetaData metadata_svc(connect, *loader);
+      metadata_svc.connect(cond::ReadWrite);
+      std::string token=metadata_svc.getToken(tag);
+      if( token.empty() ) {
+	std::cout<<"non-existing tag "<<tag<<std::endl;
+	return 11;
+      }
+      cond::DBSession session(connect,catalog);
+      cond::IOVService iovservice(session);
+      cond::IOVEditor* ioveditor=iovservice.newIOVEditor(token);
+      session.connect(cond::ReadWrite);
+      session.startUpdateTransaction();
+      ioveditor->deleteEntries();
+      session.commit();
+      session.disconnect();
+      metadata_svc.deleteEntryByTag(tag);
+      metadata_svc.disconnect();
+      delete ioveditor;
+    }catch(cond::Exception& er){
+      std::cout<<er.what()<<std::endl;
+    }catch(std::exception& er){
+      std::cout<<er.what()<<std::endl;
+    }catch(...){
+      std::cout<<"Unknown error"<<std::endl;
+    }
+  }
+  delete loader;
+  return 0;
+}
